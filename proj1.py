0eb13ff7523dd032a674b62a8fd84dc0968de929
Matthew Kelleher <Matthew.kelleher10@comcast.net>
2023-03-16 21:10:45 -0400

    Added data structures, global variables, and pipeline cycle

--- proj1.py
+++ proj1.py
@@ -1,34 +1,104 @@
 import sys
 
+#Data Structures
+map_table = [-1]*32
+ready_table = []
+free_list = []
+reorder_buffer = []
+instructions = []
+
+#Global Variables 
+num_reg = 0
+issue_width = 0 
+cycle = 0
+commited = 0
+fetch_index = 0
+icount = 0
+
+#Scheduling Queues
+fetch = []
+decode = []
+rename = []
+dispatch = []
+issue = []
+write_back = []
+commit = []
+
 #Main method
 def main():
+    global icount
+    global commited
+    global instructions
+    global cycle
+
     #Read input file
     input = sys.argv[1]
-    num_reg, issue_width, instructions = read_file(input)
+    read_file(input)
+
+    #Pipeline
+    icount = len(instructions)
+    while commited<icount:
+        Commit()
+        WB()
+        Issue()
+        Dispatch()
+        Rename()
+        Decode()
+        Fetch()
+        cycle+=1
 
     #Write output to 'out.txt'
-    output = open('out.txt', 'w')
-    output.write(str(num_reg) + '\n' + str(issue_width) + '\n' + str(instructions))
+    open('out.txt', 'w').close
+    output = open('out.txt', 'a')
+    for instruction in instructions:
+        cycle_num = instruction[4]
+        output.write(','.join(map(str,cycle_num)) + '\n')
     output.close()
 
 #Reads input file
 def read_file(file):
     with open(file, 'r') as input:
 
-        # Reads the first for the number of physical registers and the issue width
+        # Reads the number of physical registers and the issue width
         num_reg, issue_width = map(int, input.readline().split(','))
 
-        # Check if the number of physical registers is greater than 32
-        if num_reg <= 32:
-            return None
+        #Populates data structures as needed
+        ready_table = [0]*num_reg
 
         # Parse each instruction and add it to the instructions list
-        instructions = []
         for line in input:
-            instruction = line.split(',')
+            instruction = line.strip().split(',')
+            instruction.append([-1]*7)
             instructions.append(instruction)
 
-    return num_reg, issue_width, instructions
+    return
+
+def Commit():
+    global commited
+
+    commited+=1
+    return
+
+def WB():
+    return
+
+def Issue():
+    return
+
+def Dispatch():
+    return
+
+def Rename():
+    return
+
+def Decode():
+    return
+
+def Fetch():
+    global fetch_index
+
+    fetch_index+=issue_width
+    return
 
 #Calls main() on run
 if __name__ == '__main__':
